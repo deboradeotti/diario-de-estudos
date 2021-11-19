5a1c2ec831a982d2c9a9e36fddc7924f5eec44a4
Debora Deotti <debora.deotti@gmail.com>
2021-11-18 21:28:08 -0300

    Conserta ArgumentError

--- lib/study_diary.rb
+++ lib/study_diary.rb
@@ -8,53 +8,19 @@ SAIR = 4
 
 puts "Bem-vindo ao seu diário de estudos!"
 
-def menu()
-puts "[#{CADASTRAR_ITEM}] Cadastrar um item para estudar"
-puts "[#{VISUALIZAR_ITENS}] Ver todos os itens cadastrados"
-puts "[#{BUSCAR_ITEM}] Buscar um item de estudo"
-puts "[#{SAIR}] Sair"
-print "Escolha uma opção: "
-return gets.to_i()
-end
-
-def cadastrar_item()
-  puts "Digite o título do item: "
-  titulo = gets.chomp() 
-  puts "Digite a categoria do item: "
-  categoria = gets.chomp()
-  item = StudyItem.new(titulo: titulo, categoria: categoria)
-  puts
-  puts "Item #{titulo} da categoria #{categoria} cadastrado com sucesso."
-  puts
-  return item
-end
-
-def imprimir_itens(itens)
-    puts
-    puts "========== Itens cadastrados =========="
-    itens.each do |item|
-        puts "#{titulo} - #{categoria}"
-    end
-    puts
-    if itens.empty?
-        puts "Nenhum item cadastrado."
-        puts
-    end
-end
-
-opcao = menu()
+opcao = StudyItem.menu()
 
 itens = []
 
 loop do
 
   if(opcao == CADASTRAR_ITEM)
-    itens << cadastrar_item()
+    itens << StudyItem.cadastrar_item()
   elsif(opcao == VISUALIZAR_ITENS)
-    imprimir_itens(itens)
-    
-    #Adicionar função buscar
-
+    StudyItem.imprimir_itens(itens)
+  elsif(opcao == BUSCAR_ITEM)
+    # Criar um novo array só com os que correspondem e passar na função imprimir_itens()
+    StudyItem.imprimir_itens(itens)
   elsif(opcao == SAIR)
     break
   else
@@ -62,7 +28,7 @@ loop do
     puts
   end
 
-  opcao = menu()
+  opcao = StudyItem.menu()
 
 end
 
--- lib/study_item.rb
+++ lib/study_item.rb
@@ -9,4 +9,37 @@ class StudyItem
         @categoria = categoria
     end
 
+    def self.menu()
+        puts "[#{CADASTRAR_ITEM}] Cadastrar um item para estudar"
+        puts "[#{VISUALIZAR_ITENS}] Ver todos os itens cadastrados"
+        puts "[#{BUSCAR_ITEM}] Buscar um item de estudo"
+        puts "[#{SAIR}] Sair"
+        print "Escolha uma opção: "
+        return gets.to_i()
+    end
+        
+    def self.cadastrar_item()
+        puts "Digite o título do item: "
+        titulo = gets.chomp() 
+        puts "Digite a categoria do item: "
+        categoria = gets.chomp()
+        puts
+        puts "Item #{titulo} da categoria #{categoria} cadastrado com sucesso."
+        puts
+        return StudyItem.new(titulo: titulo, categoria: categoria)
+    end
+        
+    def self.imprimir_itens(itens)
+        puts
+        puts "========== Itens cadastrados =========="
+        itens.each do |item|
+            puts "#{item.titulo} - #{item.categoria}"
+        end
+        puts
+        if itens.empty?
+            puts "Nenhum item cadastrado."
+            puts
+        end
+    end
+
 end
