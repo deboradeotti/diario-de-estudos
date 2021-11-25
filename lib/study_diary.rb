987dfa5372159c5e51a16bf402e48f7c53571215
Debora Deotti <debora.deotti@gmail.com>
2021-11-25 17:52:48 -0300

    Organizando o código

--- lib/study_diary.rb
+++ lib/study_diary.rb
@@ -1,36 +1,62 @@
+require 'io/console'
 require_relative 'study_item.rb'
 require_relative 'category.rb'
 
-CADASTRAR_ITEM = 1
-VISUALIZAR_ITENS = 2
-BUSCAR_ITEM = 3
-SAIR = 4
+INSERT       = 1
+DISPLAY_ALL  = 2
+SEARCH       = 3
+MARK_AS_DONE = 4
+EXIT         = 5
 
-puts "Bem-vindo ao seu diário de estudos!"
+puts 'Bem-vindo ao seu diário de estudos!'
 
-opcao = StudyItem.menu()
+# Limpa o terminal
 
-itens = []
+def clear 
+  system 'clear'
+end
+
+def wait_keypress
+  puts
+  puts 'Pressione qualquer tecla para continuar.'
+
+  # Pega apenas um caractere (qualquer um)
+  STDIN.getch 
+end
+
+def wait_and_clear
+  wait_keypress
+  clear
+end
+
+clear 
+puts welcome
+
+opcao = StudyItem.menu
+
+study_items = []
+search = []
 
 loop do
-
-  if(opcao == CADASTRAR_ITEM)
-    itens << StudyItem.cadastrar_item()
-  elsif(opcao == VISUALIZAR_ITENS)
-    StudyItem.imprimir_itens(itens)
-  elsif(opcao == BUSCAR_ITEM)
-    # Criar um novo array só com os que correspondem e passar na função imprimir_itens()
-    StudyItem.imprimir_itens(itens)
-  elsif(opcao == SAIR)
+  case option
+  when INSERT
+    # << = shovel
+    study_items << StudyItem.insert_item
+    wait_and_clear 
+  when DISPLAY_ALL
+    StudyItem.display_itens(study_items)
+  when SEARCH
+    StudyItem.search_item(study_items)
+  when EXIT
     break
   else
     puts "Opção inválida."
     puts
+    wait_and_clear
   end
-
-  opcao = StudyItem.menu()
-
+  wait_and_clear
+  opcao = StudyItem.menu
 end
 
 puts
-puts "Obrigado por usar o diário de estudos, até logo!"
+puts "Obrigado por usar o Diário de Estudos, até logo!"
--- lib/study_item.rb
+++ lib/study_item.rb
@@ -1,45 +1,79 @@
 class StudyItem
-    attr_accessor :titulo, :categoria
+    attr_accessor :title, :category, :done
 
     require_relative 'study_diary.rb'
     require_relative 'category.rb'
 
-    def initialize(titulo: 'Sem título', categoria: Category.new)
-        @titulo = titulo
-        @categoria = categoria
+    def initialize(title:, category:)
+        @title = title
+        @category = category
+        @done = false 
     end
 
-    def self.menu()
-        puts "[#{CADASTRAR_ITEM}] Cadastrar um item para estudar"
-        puts "[#{VISUALIZAR_ITENS}] Ver todos os itens cadastrados"
-        puts "[#{BUSCAR_ITEM}] Buscar um item de estudo"
-        puts "[#{SAIR}] Sair"
+    def self.menu
+
+        # Heredoc: sintaxe para criar múltiplas linhas de texto
+        # <<-HEREDOC -> considera a indentação do texto na hora do output
+        # <<~HEREDOC -> "Squiggly Heredoc" -> não considera a indentação do texto
+
+        puts <<~MENU 
+        ---------------------------------------------------------
+        [#{CADASTRAR_ITEM}] Cadastrar um item para estudar
+        [#{VISUALIZAR_ITENS}] Ver todos os itens cadastrados
+        [#{BUSCAR_ITEM}] Buscar um item de estudo
+        [#{SAIR}] Sair
+        ---------------------------------------------------------
+        MENU
         print "Escolha uma opção: "
-        return gets.to_i()
+        gets.to_i
     end
-        
-    def self.cadastrar_item()
+      
+    def self.insert_item
         puts "Digite o título do item: "
-        titulo = gets.chomp() 
+        titulo = gets.chomp
         puts "Digite a categoria do item: "
-        categoria = gets.chomp()
+        categoria = gets.chomp
         puts
         puts "Item #{titulo} da categoria #{categoria} cadastrado com sucesso."
         puts
         return StudyItem.new(titulo: titulo, categoria: categoria)
     end
         
-    def self.imprimir_itens(itens)
+    def self.display_items(collection)
         puts
         puts "========== Itens cadastrados =========="
-        itens.each do |item|
-            puts "#{item.titulo} - #{item.categoria}"
+        itens.each.with_index(1) do |item, index|
+            puts "##{index} - #{item.titulo} - #{item.categoria}#{' - Finalizado' if item.done == true} "
         end
         puts
-        if itens.empty?
-            puts "Nenhum item cadastrado."
-            puts
+
+        # Não precisamos usar if/else porque o each já não roda se o array estiver vazio
+        puts "Nenhum item encontrado." if collection.empty?
+        puts
+        wait_and_clear
+    end
+      
+      
+    def self.search_item(collection)
+        puts "Digite o termo que deseja buscar: "
+        term = gets.chomp
+
+        # O filter é um alias do select
+        found_items = collection.filter do |item|
+            item.titulo.include? term
         end
+        display_items(found_items)
+        wait_and_clear
     end
 
+    def self.mark_as_done
+        not_finalized = StudyItem.undone
+        display_items(not_finalized)
+        return if not_finalized.empty?
+      
+        print 'Digite o número que deseja finalizar: '
+        index = gets.to_i
+        not_finalized[index - 1].done!
+    end
+      
 end
